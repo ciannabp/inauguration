9e6d5457128987a6f8847bbaa6318e40a2fb0b46
Cianna Bedford-Petersen <34047206+ciannabp@users.noreply.github.com>
2021-01-20 12:39:03 -0800

    Add files via upload
    initial commit

---
+++ R/colors.R
@@ -0,0 +1,64 @@
+#' Complete list of palettes
+#'
+#' Use \code{\link{inauguration_palette}} to construct palettes of desired length.
+#'
+#' @export
+inauguration_palettes <- list(
+  inauguration_2021 = c("#5445b1", "#749dae", "#f3c483", "#5c1a33", "#cd3341", "#f7dc6a")
+)
+
+#' Inauguration palette generator
+#'
+#'
+#'
+#' @param n Number of colors desired. Most palettes now only
+#'   have 5 to 8 colors. Most color
+#'   schemes are derived from \href{http://poppygall.com/blog/2011/07/25/color-inspiration-national-park-wpa-posters/}{Color Inspiration – National Park WPA Posters}.
+#'   If omitted, uses all colours.
+#' @param name Name of desired palette. Choices are:
+#'   \code{inauguration_2021}
+#'   @importFrom graphics rgb rect par image text
+#' @return A vector of colours.
+#' @export
+#' @keywords colors
+#' @examples
+#' inauguration_2021_palette("inauguration_2021")
+#' inauguration_2021("inauguration_2021", 3)
+
+inauguration_palette <- function(name, n) {
+
+
+  pal <- inauguration_palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (n > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
+  }
+
+  out <- pal[1:n]
+
+  structure(out, class = "palette", name = name)
+}
+
+#' @export
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "Lato", col = "#32373D")
+}
+
+
+
