1e5b2cc11473300e58e0106b6af875134a1e1d47
Cianna Bedford-Petersen <34047206+ciannabp@users.noreply.github.com>
2021-01-22 10:54:47 -0800

    Add Bernie mittens

--- R/colors.R
+++ R/colors.R
@@ -7,7 +7,8 @@
 #'
 #' @export
 inauguration_palettes <- list(
-  inauguration_2021 = c("#5445b1", "#749dae", "#f3c483", "#5c1a33", "#cd3341","#f7dc6a")
+  inauguration_2021 = c("#5445b1", "#749dae", "#f3c483", "#5c1a33", "#cd3341","#f7dc6a"),
+  inauguration_2021_bernie = c("#5445b1", "#749dae", "#f3c483", "#5c1a33", "#cd3341","#f7dc6a","#6B4B3B")
 )
 
 #' Inauguration palette generator
